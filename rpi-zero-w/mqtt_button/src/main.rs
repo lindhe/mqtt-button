9a1a297aa4b209de30308a4a9d39d8e0618164f4
Andreas Lindhé <andreas@lindhe.io>
2023-01-17 19:40:24 +0100

    Replicate the CLI args

--- rpi-zero-w/mqtt_button/src/main.rs
+++ rpi-zero-w/mqtt_button/src/main.rs
@@ -1,22 +1,42 @@
 use clap::Parser;
 
-/// Simple program to greet a person
+/// Sends an MQTT message upon buttonpress
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    /// Hostname to MQTT server
+    #[arg(short = 'H', long)]
+    hostname: String,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// GPIO pin for the button
+    #[arg(short, long)]
+    gpio_pin: u8,
+
+    /// Invert button state (default: closed circuit == pressed)
+    #[arg(long)]
+    inverted: bool,
+
+    /// Payload for MQTT message when button is pressed
+    #[arg(short, long, default_value = "PRESSED")]
+    pressed_message: String,
+
+    /// Payload for MQTT message when button is released
+    #[arg(short, long, default_value = "RELEASED")]
+    released_message: String,
+
+    /// Use keyboard input instead of GPIO button
+    #[arg(long)]
+    mocked: bool,
+
+    /// MQTT topic to publish to
+    #[arg(short, long)]
+    topic: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    for _ in 0..args.gpio_pin {
+        println!("Hello {}!", args.hostname)
     }
 }
